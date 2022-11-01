4da93c5b116300ebdff7a5c2f5445d8f08f2d18c
NMoz88 <nesmartinez2@gmail.com>
2022-11-01 02:45:54 +0000

    aun no se muestra

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function excuseGenerator(who, what, when) {
+    let rand1 = Math.floor(Math.random() * who.length);
+    let rand2 = Math.floor(Math.random() * what.length);
+    let rand3 = Math.floor(Math.random() * when.length);
+
+    console.log(who[rand1] + " " + what[rand2] + " " + when[rand3]);
+  }
+
+  function onLoad() {
+    var excusa = document.getElementById("excuse");
+    excusa.innerHTML = excuseGenerator(who, what, when);
+  }
 };
